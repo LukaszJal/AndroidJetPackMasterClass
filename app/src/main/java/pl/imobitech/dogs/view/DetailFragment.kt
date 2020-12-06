8e84279a30f2cbe87e739ab04710a69d7aa2cb9e
LukaszJal <lukasz@jaloszynski.eu>
2020-12-06 14:54:03 +0100

    Palette.

--- app/src/main/java/pl/imobitech/dogs/view/DetailFragment.kt
+++ app/src/main/java/pl/imobitech/dogs/view/DetailFragment.kt
@@ -1,17 +1,25 @@
 package pl.imobitech.dogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_detail.*
 import pl.imobitech.dogs.R
 import pl.imobitech.dogs.databinding.FragmentDetailBinding
+import pl.imobitech.dogs.model.DogPalette
 import pl.imobitech.dogs.util.getProgressDrawable
 import pl.imobitech.dogs.util.loadImage
 import pl.imobitech.dogs.viewmodel.DetailViewModel
@@ -51,9 +59,35 @@ class DetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(viewLifecycleOwner, { dog ->
             dog?.let {
-              dataBinding.dog = dog
+                dataBinding.dog = dog
+
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
+
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>(){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?:0
+                            val myPalette = DogPalette(intColor)
+                            dataBinding.palette = myPalette
+
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+            })
     }
 
 }
